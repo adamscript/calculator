f70e4a851273afbcb67b05128bf7a9cef4744d01
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-16 14:19:41 +0800

    Added basic operator functions

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, operator, num2){
+    switch(operator){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
